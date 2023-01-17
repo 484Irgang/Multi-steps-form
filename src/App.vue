f059c877c38ebf5a7ab12c6e0b4d9db498d860ca
Guilherme Irgang <484gui@gmail.com>
2023-01-17 19:40:43 -0300

    iniciando o projeto

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,36 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  import PersonalInfo from "./components/PersonalInfo.vue"
+
+  export default{
+    components: {PersonalInfo}
+  }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <PersonalInfo/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .container{
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    padding: 2%;
+    background-color: #EEF5FF;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+      }
+  .container > div{
+    width: 100%;
+    max-width: 1100px;
+    height: 100%;
+    max-height: 600px;
+    background-color: white;
+    border-radius: 14px;
+    box-shadow: 4px 6px 30px -20px rgba(0, 0, 0, 0.5);
+  }
 </style>
