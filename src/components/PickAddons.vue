2e5dd7e5bcea0cbebfc3f6f169c7f6c9b340ad45
484Irgang <484gui@gmail.com>
2023-04-08 23:39:26 -0300

    Finalizando a interface e interação de seleção dos Add-ons

--- src/components/PickAddons.vue
+++ src/components/PickAddons.vue
@@ -7,20 +7,22 @@
         },
         methods: {
             choiceAdd(e,i){
+        
                 if(e.target.type == "checkbox")
                     e.preventDefault();
-                console.log(e.target.defaultChecked);
-                var input_check = document.querySelectorAll(".add-on_box .check input");
+
+                var input_check = document.querySelectorAll(".add-on_box input");
                 input_check[i].defaultChecked = !input_check[i].defaultChecked;
+
+                console.log(e.target);
+
+                input_check[i].className = input_check[i].defaultChecked ? "checked" : "";
+
                 var addon_box = document.querySelectorAll(".add-on_box");
                 addon_box[i].style.backgroundColor = input_check[i].defaultChecked ? "hsl(217, 100%, 97%)" : "transparent";
             }
         },
-        computed: {
-            checkedOrNot(){
-                return false;
-            }
-        }
+       
     }
 </script>
 
@@ -32,26 +34,19 @@
         </div>
 
         <div class="add-on_box" @click="(e) => choiceAdd(e,0)">
-            <label class="check">
-                <span :class="{checked: checkedOrNot}"></span>
-                <input type="checkbox"/>
-            </label>
+            <input type="checkbox"/>
             <label class="alt"><p>{{ add_ons[0].type }}</p><p>Acess to multiplayer games</p></label>
             <span>+${{ add_ons[0].value }}/mo</span>
         </div>
 
         <div class="add-on_box" @click="(e) => choiceAdd(e,1)">
-            <label class="check">
-                <input type="checkbox"/>
-            </label>
+            <input type="checkbox"/>
             <label class="alt"><p>{{ add_ons[1].type }}</p><p>Extra 1 TB of cloud save</p></label>
             <span>+${{ add_ons[1].value }}/mo</span>
         </div>
 
         <div class="add-on_box" @click="(e) => choiceAdd(e,2)">
-            <label class="check">
-                <input type="checkbox"/>
-            </label>
+            <input type="checkbox"/>
             <label class="alt"><p>{{ add_ons[2].type }}</p><p>Custom theme on your profile</p></label>
             <span>+${{ add_ons[2].value }}/mo</span>
         </div>
@@ -96,30 +91,19 @@
     border-radius: 6px;
     cursor: pointer;
 }
-.add-on_box .check{
-    position: relative;
-    background-color: white;
-    margin-right: 0.8em;
-    border-radius: 4px;
-    height: 16px;
-    width: 16px;
-}
-.add-on_box .check input{
+.add-on_box input{
     appearance: none;
-    width: 100%;
-    height: 100%;
+    margin-right: 0.8em;
+    width: 16px;
+    height: 16px;
     border-radius: 4px;
     border: 0.5px solid #DBDADF;
     cursor: pointer;
 }
 .add-on_box .checked{
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    border-radius: 4px;
-    cursor: pointer;
     clip-path: polygon(0% 0%,100% 0%, 100% 100%, 46% 100%, 46% 74%, 89% 32%, 76% 21%, 44% 53%, 25% 41%, 13% 54%, 46% 74%, 46% 100%, 0% 100%);
     background-color: #4C45FF;
+    z-index: 2;
 }
 .add-on_box .alt{
      cursor: pointer;
@@ -137,10 +121,5 @@
     color: #473dff;
     margin-left: auto;
     font-size: 0.9em;
-}
-.teste{
-    width: 500px;
-    height: 500px;
-    background-color: aqua;
 }
 </style>
